edc3cd3517274b33d561580f505d9f4eaacf32eb
k4-100 <k4-100@protonmail.com>
2022-12-19 16:00:57 +0100

    dodano wyciąganie danych z productów do listy

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-import json
 import requests
 
+from pprint import pprint
 
 headers = {'Content-Type': "application/json", 'Accept': "application/json"}
 
 key='WECP867LZWGUGCVF4PJGA5W83GULG6M4'
-# api=f'http://{key}@localhost/prestashop_1_6/prestashop/api?output_format=JSON&io_format=JSON'
-api='http://localhost/prestashop_1_6/prestashop/api?output_format=JSON&io_format=JSON'
+api='http://localhost/prestashop_1_6/prestashop/api'
 
 
-response = requests.get( url=api, headers=headers, auth=(key,''))
+
+def get_products(main_url, section_url, headers, auth):
+    def get_product(item):
+        id = item['id']
+        res = requests.get( url=f'{main_url}/{section_url}/{id}?output_format=JSON&io_format=JSON', headers=headers, auth=auth ).json()
+        return res
+
+
+    # pobiera dane i zbiera do jsona
+    response = requests.get( url=f'{main_url}/{section_url}?output_format=JSON&io_format=JSON', headers=headers, auth=auth ).json()["products"]
+    responses = []
+    
+    for item in response:
+        responses.append( get_product(item) )
+
+
+    return responses
+
 
 if __name__ == '__main__':
-    print(response.json())
+    pprint( get_products( main_url=api, section_url='products', headers=headers, auth=(key,'')) )
 else:
     print("Run from import")
