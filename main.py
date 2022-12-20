548c90cc6f3455ebb8eaa98a4fcaeeb09a4b987c
k4-100 <k4-100@protonmail.com>
2022-12-20 09:59:15 +0100

    Basic sql connection and query

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 
+import mysql.connector
+from pprint import pprint
+
+
+def main():
+    db = 'prestashop_1_6'
+
+    cnx = mysql.connector.connect(
+            user='root', 
+            password='',
+            host="127.0.0.1",
+            database=db
+    )
+    result = cnx._execute_query("SELECT * FROM ps_product");
+    cnx.close()
+
+    pprint(result)
+
+    print('text')
+
 
 if __name__ == '__main__':
+    main()
 else:
     print("Run from import")
