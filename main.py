8da1450b672c219942c6eaeae94c13e45e00f109
k4-100 <k4-100@protonmail.com>
2022-12-20 12:53:39 +0100

    Added file writing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import tablib
 import mysql.connector
 from pprint import pprint
 
@@ -12,13 +13,33 @@ def main():
             host="127.0.0.1",
             database=db
     )
-    result = cnx._execute_query("SELECT * FROM ps_product");
+    result = {}
+    result = cnx._execute_query("SELECT * FROM ps_product")
     cnx.close()
+        
 
-    pprint(result)
+    data = tablib.Dataset()
+    # collection of names
 
-    print('text')
 
+    columns = result['columns']
+    headers = []
+    for item in columns:
+        headers.append(item[0])
+    
+    for x in range(0, len(columns)):
+        row = []
+        for y in range(1, len(columns[0])):
+            row.append( columns[x][y] )
+            if y == len(columns[0]) - 1:
+                data.append_col( row, header='s' )
+
+        
+    data.headers = headers
+    # pprint( data.dict )
+    f = open("test.xls", "wb")
+    f.write( data.export('xls') )
+    f.close()
 
 if __name__ == '__main__':
     main()
